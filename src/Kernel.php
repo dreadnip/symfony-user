f0bd7c805c149d2dc0400bbf7af91acc5bfdce32
Sander De la Marche <sander@sumocoders.be>
2021-04-16 14:18:36 +0200

    Re-load yaml config in our kernel

--- src/Kernel.php
+++ src/Kernel.php
@@ -13,25 +13,25 @@ class Kernel extends BaseKernel
 
     protected function configureContainer(ContainerConfigurator $container): void
     {
-        $container->import('../config/{packages}/*.php');
-        $container->import('../config/{packages}/'.$this->environment.'/*.php');
+        $container->import('../config/{packages}/*.yaml');
+        $container->import('../config/{packages}/'.$this->environment.'/*.yaml');
 
         if (is_file(\dirname(__DIR__).'/config/services.yaml')) {
             $container->import('../config/services.yaml');
             $container->import('../config/{services}_'.$this->environment.'.yaml');
-        } elseif (is_file($path = \dirname(__DIR__).'/config/services.php')) {
+        } elseif (is_file($path = \dirname(__DIR__).'/config/services.yaml')) {
             (require $path)($container->withPath($path), $this);
         }
     }
 
     protected function configureRoutes(RoutingConfigurator $routes): void
     {
-        $routes->import('../config/{routes}/'.$this->environment.'/*.php');
-        $routes->import('../config/{routes}/*.php');
+        $routes->import('../config/{routes}/'.$this->environment.'/*.yaml');
+        $routes->import('../config/{routes}/*.yaml');
 
         if (is_file(\dirname(__DIR__).'/config/routes.yaml')) {
             $routes->import('../config/routes.yaml');
-        } elseif (is_file($path = \dirname(__DIR__).'/config/routes.php')) {
+        } elseif (is_file($path = \dirname(__DIR__).'/config/routes.yaml')) {
             (require $path)($routes->withPath($path), $this);
         }
     }
