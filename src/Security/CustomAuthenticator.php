70afea85317bca164d8d05dda654cf39237a3485
Sander De la Marche <sander@sumocoders.be>
2021-04-16 14:05:49 +0200

    Make sure there is always a RememberMe badge present in the auth Passport

--- src/Security/CustomAuthenticator.php
+++ src/Security/CustomAuthenticator.php
@@ -10,6 +10,7 @@ use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Security;
 use Symfony\Component\Security\Http\Authenticator\AbstractLoginFormAuthenticator;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\CsrfTokenBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\RememberMeBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\PasswordCredentials;
 use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
@@ -44,7 +45,8 @@ class CustomAuthenticator extends AbstractLoginFormAuthenticator
             new PasswordCredentials($credentials['password']),
             [
                 new CsrfTokenBadge('login', $credentials['token']),
-            ]
+                new RememberMeBadge(),
+            ],
         );
     }
 
